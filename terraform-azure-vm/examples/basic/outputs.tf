2fa725b63fdf00185cfcc03e1afbfb7c51b3f0ed
shazmabanu <136704096+shazmabanu@users.noreply.github.com>
2025-02-14 01:29:23 +0530

    Delete terraform-azure-vm directory

--- terraform-azure-vm/examples/basic/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_id" {
-  description = "The ID of the virtual machine."
-  value       = module.azure_vm.vm_id
-}
-
-output "vm_public_ip" {
-  description = "The public IP address of the VM."
-  value       = module.azure_vm.vm_public_ip
-}
